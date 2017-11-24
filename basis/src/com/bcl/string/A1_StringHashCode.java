dea05ddc4c9bd77547e60f2eabf10b1adea090f0
bcl1990 <549157061@qq.com>
2017-11-24 16:26:46 +0800

    2

--- basis/src/com/bcl/string/A1_StringHashCode.java
+++ basis/src/com/bcl/string/A1_StringHashCode.java
@@ -2,7 +2,10 @@ package com.bcl.string;
 /**
  * 
  * @author bcl
- *	String hashCode实现方式:从第一个字符ASCII码*31，加第二个字符和再*31，一直循环，加到最后一个
+ *	String hashCode实现方式:从第一个字符ASCII码*31，加第二个字符和再*31，一直循环，加到最后一个.
+ *	开始取缓存的h， int h = hash;
+        if (h == 0 && value.length > 0)
+        	如果hash没计算过先计算，最后赋值给hash
  */
 public class A1_StringHashCode {
 	public static void main(String[] args) {
--- basis/src/com/bcl/test/Test2.java
+++ basis/src/com/bcl/test/Test2.java
@@ -1,22 +1,45 @@
 package com.bcl.test;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
 
-public class Test2 {
-	public static void main(String[] args) {
-		Random random =new Random(1000);
-		Random random1 =new Random(1000);
-		int arr[]=new int[10];
-		int arr1[]=new int[10];
-		for(int i=0;i<10;i++) {
-			arr[i]=random.nextInt(2);
-		}
-		for(int i=0;i<10;i++) {
-			arr1[i]=random1.nextInt(2);
-		}
-		System.out.println(Arrays.toString(arr));
-		System.out.println(Arrays.toString(arr1));
-		System.out.println(Math.random());
-	}
-}
+public class Test2 {  
+    public static void main(String[] args) {  
+        /*WeakReference<People>reference=new WeakReference<People>(new People("zhouqian",20));  
+        System.out.println(reference.get());  
+        System.gc();//通知GVM回收资源  
+        System.out.println(reference.get()); */ 
+    	People p1=new People("zhouqian", 20);
+    	People p2=new People("11", 22);
+    	ReferenceQueue<People> rq= new ReferenceQueue<>();
+    	SoftReference<People> reference=new SoftReference<People>(p2);
+    	PhantomReference<People> pf =new PhantomReference<People>(p1, rq);
+    	PhantomReference<People> pf2 =new PhantomReference<People>(p2, rq);
+    	p1=null;
+    	
+        System.out.println(reference.get()); 
+        byte[] b=new byte[1000*1000*1];
+        System.gc();//通知GVM回收资源  
+        System.out.println(reference.get());
+        System.out.println(reference.isEnqueued());
+        System.out.println(pf.isEnqueued());//true
+        System.out.println(pf2.isEnqueued());//false
+        //System.gc();//通知GVM回收资源  
+        //System.out.println(reference.get());
+    }  
+}  
+class People{  
+    public String name;  
+    public int age;  
+    byte[] b;
+    public People(String name,int age) {  
+        this.name=name;  
+        this.age=age;  
+       b=new byte[1000*1000*333];
+    }  
+    @Override  
+    public String toString() {  
+        return "[name:"+name+",age:"+age+"]";  
+    }  
+}  
